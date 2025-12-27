36456b2ea18ab85b7fc6942b6f5e208b09e3ea29
gzaxx <gzaxxor@gmail.com>
2025-12-27 11:11:26 +0100

    Added postgres

--- Invoicer.AppHost/AppHost.cs
+++ Invoicer.AppHost/AppHost.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var database = builder.AddPostgres("postgres")
+    .WithPgAdmin(options => options.WithHostPort(6001));
+database.AddDatabase("Invoicer");
+
 builder.AddViteApp(name: "Invoicer-App", appDirectory: "../frontend");
 
 builder.Build().Run();
